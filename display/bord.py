a64464df8132a7d56eb1bd3b2670078ea97de3c1
idonacmias <idonacmi@gmail.com>
2023-08-30 14:23:55 +0300

    generelize buttons draw func

--- display/bord.py
+++ display/bord.py
@@ -1,4 +1,6 @@
 from display import colors_palet, city, card, tokens, player, bottons, bord
+from .constances import BUTTONS_POINTS, BUTTONS_TEXTS
+
 
 def draw_bord(screen, font, corent_page, cities, players, player_discard):
    if corent_page == 'map':
@@ -6,6 +8,7 @@ def draw_bord(screen, font, corent_page, cities, players, player_discard):
 
    if corent_page == 'cards':
       draw_cards(screen, font, cities, players)
+
 
 def draw_map(screen, font, cities, players, player_discard):
    screen.fill(colors_palet['PURPLE'])
@@ -18,7 +21,9 @@ def draw_map(screen, font, cities, players, player_discard):
    tokens.draw_outbreak_bar(screen, font, 0)
    tokens.draw_medicen_bar(screen)
    player.draw(cities, screen, players)
-   bottons.draw(screen, font)
+   buttons_data = zip(BUTTONS_POINTS, BUTTONS_TEXTS)
+   bottons.draw(screen, font, buttons_data)
+
 
 def draw_cities(screen, font, cities):
     for city_data in cities.values():
@@ -27,6 +32,9 @@ def draw_cities(screen, font, cities):
     for city_data in cities.values():
         city.draw(city_data, screen, font)
 
+
 def draw_cards(screen, font, cities, players):
    screen.fill(colors_palet['PURPLE'])
    card.dispaly_players_cards(screen, font, cities, players)
+   # buttons_data = zip(BUTTONS_POINTS, BUTTONS_TEXTS)
+   # bottons.draw(screen, font, buttons_data)
--- display/bottons.py
+++ display/bottons.py
@@ -1,16 +1,16 @@
 import pygame
 
-from .constances import BUTTONS_POINTS,BUTTONS_TEXTS, BUTTON_WHIDTH, BUTTON_HIGHT
+from .constances import BUTTON_WHIDTH, BUTTON_HIGHT
 from .color import colors_palet
 
 
-def draw(screen, font):
-    BUTTONS_DATA = zip(BUTTONS_POINTS, BUTTONS_TEXTS)
-    for point, text in BUTTONS_DATA:
+def draw(screen, font, points_text_zip):
+    for point, text in points_text_zip:
         draw_squer(screen, point)
         text_render = font.render(text, True, colors_palet['BLACK'])
         text_point = (point[0] + 10, point[1])    
         screen.blit(text_render, text_point)
+
 
 def draw_squer(screen, point):
     square_points = [point, (point[0] + BUTTON_WHIDTH, point[1]), (point[0] + BUTTON_WHIDTH, point[1] + BUTTON_HIGHT), (point[0], point[1] + BUTTON_HIGHT)]
