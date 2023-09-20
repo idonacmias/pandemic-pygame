766468a0642eeaeaa3714f4b8e10052e70efc614
idonacmias <idonacmi@gmail.com>
2023-09-20 11:49:42 +0300

    add button cure disease, builed research station button call a dedicated function, add reserch statiom functionnot handeling player action.

--- events/bord_map.py
+++ events/bord_map.py
@@ -2,7 +2,6 @@ from display import card, CITY_RADIUS, bottons
 from display import MAP_BUTTONS_POINTS,MAP_BUTTONS_TEXTS, BUTTON_WHIDTH, BUTTON_HIGHT
 
 
-
 def clicked_on_city(cities, corent_player, mouse_point, bord_state):
     min_radius = CITY_RADIUS
     closest_city_name = None
@@ -35,21 +34,23 @@ def click_on_botton(cities, corent_player, botton_clicked, bord_state):
     elif botton_clicked == 'discover cure': print('cure discoverd')
 
     elif botton_clicked == 'builed research station':
-        corent_city = cities[corent_player.corent_city_name]
-        if corent_city.research_station: return None
+        click_builed_research_station(bord_state, corent_player, cities)
 
-        if bord_state.num_research_station <= 0: remove_city_research_station(bord_state, cities) 
 
-        add_city_research_station(corent_city, bord_state, corent_player)
+def click_builed_research_station(bord_state, corent_player, cities):
+    corent_city = cities[corent_player.corent_city_name]
+    if not corent_city.research_station: 
+        corent_player.actions -= 1
+        add_city_research_station(bord_state, corent_city, cities)
 
-def remove_city_research_station(bord_state, cities):
-    oldest_research_station = bord_state.research_stations.pop(0)
-    cities[oldest_research_station].research_station = False
+def add_city_research_station(bord_state, city, cities):
+    if bord_state.num_research_station <= 0: remove_city_research_station(bord_state, cities) 
+    city.research_station = True
+    bord_state.research_stations.append(city.name)
+    bord_state.num_research_station -= 1
+        
+def remove_city_research_station(bord_state, cities, research_station_cuonter=0):
+    research_station_name = bord_state.research_stations.pop(research_station_cuonter)
+    cities[research_station_name].research_station = False
     bord_state.num_research_station += 1
 
-
-def add_city_research_station(corent_city, bord_state, corent_player):
-    corent_city.research_station = True
-    bord_state.research_stations.append(corent_city.name)
-    bord_state.num_research_station -= 1
-    corent_player.actions -= 1
