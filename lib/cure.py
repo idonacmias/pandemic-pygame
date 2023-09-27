10b75f8b984d54108c64a7dc0800a40cfa13fec0
idonacmias <idonacmi@gmail.com>
2023-09-27 11:21:00 +0300

    player need 5 cards to discover cure

--- lib/cure.py
+++ lib/cure.py
@@ -1,3 +1,5 @@
+from data import NUMBER_CARDS_NEEDED_TO_CURE
+
 def discover_cure(bord_state, picked_cards, corent_player):
     if is_valid_cure(bord_state, picked_cards, corent_player):
         bord_state.cure[picked_cards[0].color.name] = 1
@@ -8,11 +10,17 @@ def discover_cure(bord_state, picked_cards, corent_player):
 
 def is_valid_cure(bord_state, picked_cards, corent_player):
     first_card = picked_cards[0]
-    if bord_state.cure[first_card.color] == 0:
-        for card in picked_cards[1:]:
-            if card.color != first_card.color or card not in corent_player.hand:
-                break
+    if bord_state.cure[first_card.color.name] != 0:
+        return False
 
-        else:
-            return True
+    number_cards_needed_to_cure = NUMBER_CARDS_NEEDED_TO_CURE
+    if len(picked_cards) != number_cards_needed_to_cure:
+        return False
+
+    for card in picked_cards[1:]:
+        if card.color != first_card.color or card not in corent_player.hand:
+            break
+
+    else:
+        return True
 
