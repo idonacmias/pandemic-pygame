9d57a1e419c9c5a9558e1633c3646182d43fb818
idonacmias <idonacmi@gmail.com>
2023-10-01 10:07:31 +0300

    palyer need card of the city to builed reserch station, and card is removed from haned afterword

--- lib/city.py
+++ lib/city.py
@@ -3,10 +3,15 @@ from display import Color
 
 def builed_research_station(bord_state, corent_player, cities, action=True):
     corent_city = corent_player.corent_city
-    if not corent_city.research_station: 
+    if (not corent_city.research_station and 
+        is_player_have_city_card(corent_player)):
+         
         if action: corent_player.actions -= 1
         add_city_research_station(bord_state, corent_city, cities)
+        remove_player_card(corent_player)
 
+def is_player_have_city_card(corent_player):
+    if corent_player.corent_city in corent_player.hand: return True
 
 def add_city_research_station(bord_state, city, cities):
     if bord_state.num_research_station <= 0: remove_city_research_station(bord_state, cities) 
@@ -14,15 +19,17 @@ def add_city_research_station(bord_state, city, cities):
     bord_state.research_stations.append(city.name)
     bord_state.num_research_station -= 1
         
-        
+
 def remove_city_research_station(bord_state, cities, research_station_cuonter=0):
     research_station_name = bord_state.research_stations.pop(research_station_cuonter)
     cities[research_station_name].research_station = False
     bord_state.num_research_station += 1
 
 
-
-
+def remove_player_card(corent_player):
+    for i, card in enumerate(corent_player.hand):
+        if card == corent_player.corent_city:
+            corent_player.hand.pop(i)
 
 
 
