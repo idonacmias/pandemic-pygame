586ba31d6d48c99953d0098e4f294a6af7497916
idonacmias <idonacmi@gmail.com>
2023-10-01 10:42:41 +0300

    builed_research_station get its own modul

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,6 @@
 from .infaction import first_infaction, infected_phase
 from .player_deck import draw_from_deck
 from .cure import discover_cure
-from .city import treat_diseasse, builed_research_station
+from .city import treat_diseasse
+from .research_station import builed_research_station
 
